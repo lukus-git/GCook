9f916d960696d756d33baac496c02304ad8dbda1
gallojunior <gallojunior@gmail.com>
2025-10-14 18:52:09 -0300

    Finalizada a camada de dados e adicionado os serviços de contexto e identidade

--- GCook/Program.cs
+++ GCook/Program.cs
@@ -1,9 +1,40 @@
+using GCook.Data;
+using GCook.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Serviço de conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("GCookDb");
+var versao = ServerVersion.AutoDetect(conexao);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(conexao, versao)
+);
+
+// Serviço de identificação do usuário
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options =>
+    {
+        options.SignIn.RequireConfirmedEmail = true;
+        options.User.RequireUniqueEmail = true;
+    }
+)
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+// Garantir que o banco exista ao executar o projeto
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+    await db.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,6 +47,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
